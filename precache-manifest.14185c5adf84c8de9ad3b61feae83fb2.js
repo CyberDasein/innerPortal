affdf899101b9cf2cbf46ee51c8e069835134da0
Alex <CyberDasein>
2020-04-30 18:35:09 +0500

    Updates

---
+++ precache-manifest.14185c5adf84c8de9ad3b61feae83fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e63bc9988096ca7c5ba61efe8b09c35",
+    "url": "/innerPortal/index.html"
+  },
+  {
+    "revision": "5b4a268b7e29cdcb186c",
+    "url": "/innerPortal/static/css/main.2f4cabe6.chunk.css"
+  },
+  {
+    "revision": "f4709f064a0125037b32",
+    "url": "/innerPortal/static/js/2.42da1681.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/innerPortal/static/js/2.42da1681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4a268b7e29cdcb186c",
+    "url": "/innerPortal/static/js/main.e70edfc2.chunk.js"
+  },
+  {
+    "revision": "e0c6373f91b0fd438a56",
+    "url": "/innerPortal/static/js/runtime-main.37de7724.js"
+  },
+  {
+    "revision": "bce5fe6da6a83ce2f045191fc51cd57d",
+    "url": "/innerPortal/static/media/unnamed.bce5fe6d.jpg"
+  }
+]);
